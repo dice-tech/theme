f86d2b10ef98b8b0fd35c922ac43fa44cd672207
Antrepriz <lakshay@antrepriz.com>
2022-07-31 00:20:53 +0530

    Automated Commit

---
+++ src/Theme/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * The only true retro theme.
+ *
+ * @version 1.0.0
+ * @author [Lakshay Jain]
+ * @author [Aakash Bhadana]
+ * @organisation [Dice Tech]
+ */
+export default function Theme({name}){
+	return (
+		<div>
+			{name}
+		</div>
+	)
+}
